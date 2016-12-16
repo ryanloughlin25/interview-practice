469ba1f1c552555788ee6de75de83c754a5978a9
Sean Grady <vedicmonk@gmail.com>
2016-12-16 13:28:40 -0800

    working solution for longest substring, but too slow

--- leetcode_problems/longest_substring_without_repeats/longest_substring_without_repeating_characters.py
+++ leetcode_problems/longest_substring_without_repeats/longest_substring_without_repeating_characters.py
@@ -1,17 +1,17 @@
-class Solution(object):
-    # First attempt, shitty brute force
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        for i in range(len(s)):
-            char_dict = {}
-
-
-
-if __name__ == "__main__":
-    string = 'test'
-    sol = Solution()
-    length = sol.lengthOfLongestSubstring(string)
-    print length
+def lengthOfLongestSubstring(string):
+    """
+    :type s: str
+    :rtype: int
+    """
+    char_set = set()
+    max_len = 0
+    for i in range(len(string)):
+        for char in string[i:]:
+            if char in char_set:
+                max_len = max(max_len, len(char_set))
+                char_set = set()
+                break
+            else:
+                char_set.add(char)
+                max_len = max(max_len, len(char_set))
+    return max_len
