5e9912624d5ae9e9931ac43f5fb1d4a68683e38d
Sean Grady <vedicmonk@gmail.com>
2016-12-17 00:17:41 -0800

    added some more simple tests

--- leetcode_problems/median_of_two_sorted_arrays/sean/tests.py
+++ leetcode_problems/median_of_two_sorted_arrays/sean/tests.py
@@ -1,14 +1,53 @@
 import unittest
-from median_of_arrays import find_median
+from median_of_arrays import find_median, find_median_single_array
 
 
-class MedianOfTwoSortedArraysTestCase(unittest.testcase):
+class MedianOfTwoSortedArraysTestCase(unittest.TestCase):
     def test_empty_arrays(self):
         array1 = []
         array2 = []
-        self.assertEqual(self.find_median(array1, array2), None)
+        self.assertEqual(find_median(array1, array2), None)
 
-    def test_one_array_one_number(self):
+    def test_array1_one_number(self):
         array1 = [5]
         array2 = []
-        self.assertEqual(self.find_median(array1, array2), 5)
+        self.assertEqual(find_median(array1, array2), 5)
+
+    def test_array2_one_number(self):
+        array1 = []
+        array2 = [5]
+        self.assertEqual(find_median(array1, array2), 5)
+
+    def test_two_single_numbers(self):
+        array1 = [4]
+        array2 = [3]
+        self.assertEqual(find_median(array1, array2), 3.5)
+
+    def test_short_asymmetric_arrays(self):
+        array1 = [1,3]
+        array2 = [2]
+        self.assertEqual(find_median(array1, array2), 2)
+
+    def test_even_array1(self):
+        array1 = [1,2,3,4]
+        array2 = []
+        self.assertEqual(find_median(array1, array2), 2.5)
+
+    def test_even_array2(self):
+        array1 = []
+        array2 = [1,2,3,4]
+        self.assertEqual(find_median(array1, array2), 2.5)
+
+    def test_odd_array1(self):
+        array1 = [1,2,3,4,5]
+        array2 = []
+        self.assertEqual(find_median(array1, array2), 3)
+
+    def test_odd_array2(self):
+        array1 = []
+        array2 = [1,2,3,4,5]
+        self.assertEqual(find_median(array1, array2), 3)
+
+
+if __name__ == '__main__':
+    unittest.main()
