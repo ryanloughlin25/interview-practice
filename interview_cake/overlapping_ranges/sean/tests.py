f134527c400519cfd5f1117cff01a54d43098cb0
Sean Grady <vedicmonk@gmail.com>
2016-12-17 15:47:04 -0800

    working O(nlogn) solution, with I think good test coverage?

---
+++ interview_cake/overlapping_ranges/sean/tests.py
@@ -0,0 +1,43 @@
+import unittest
+from overlapping_ranges import find_range_set
+
+
+class OverlappingRangesTestCase(unittest.TestCase):
+    def test_empty_list(self):
+        ranges = []
+        target = []
+        result = find_range_set(ranges)
+        self.assertEqual(result, target)
+
+    def test_example_case(self):
+        ranges = [[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]]
+        target = [[0, 1], [3, 8], [9, 12]]
+        result = find_range_set(ranges)
+        self.assertEqual(result, target)
+
+    def test_all_disjoint(self):
+        ranges = [[0,1], [10, 15], [20, 25]]
+        target = [[0,1], [10, 15], [20, 25]]
+        result = find_range_set(ranges)
+        self.assertEqual(result, target)
+
+    def test_not_sorted(self):
+        ranges = [[10, 12], [0, 1], [9, 10], [3, 5], [4,8]]
+        target = [[0, 1], [3, 8], [9, 12]]
+        result = find_range_set(ranges)
+        self.assertEqual(result, target)
+
+    def test_all_overlapping(self):
+        ranges = [[4, 6], [5, 10], [6, 11]]
+        target = [[4, 11]]
+        result = find_range_set(ranges)
+        self.assertEqual(result, target)
+
+    def test_touching(self):
+        ranges = [[0, 5], [5, 10], [10, 15]]
+        target = [[0, 15]]
+        result = find_range_set(ranges)
+        self.assertEqual(result, target)
+
+if __name__ == '__main__':
+    unittest.main()
