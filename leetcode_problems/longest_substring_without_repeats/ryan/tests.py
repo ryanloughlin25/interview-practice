b3d379cd73a72932c8cb7afd06475302b7e7a759
rloughlin <rbloughlin@gmail.com>
2016-12-16 17:21:14 -0800

    add longest substring without repeats tests

---
+++ leetcode_problems/longest_substring_without_repeats/ryan/tests.py
@@ -0,0 +1,23 @@
+import unittest
+from longest_substring import longest_substring
+
+
+class LongestSubstringTestCase(unittest.TestCase):
+    def test_empty(self):
+        self.assertEqual(longest_substring(''), '')
+
+    def test_only_repeat(self):
+        self.assertEqual(longest_substring('aaa'), 'a')
+
+    def test_no_repeat(self):
+        self.assertEqual(longest_substring('abcd'), 'abcd')
+
+    def test_simple_repeat(self):
+        self.assertEqual(longest_substring('1231234'), '1234')
+
+    def test_hard_repeat(self):
+        self.assertEqual(longest_substring('dvdf'), 'vdf')
+
+
+if __name__ == '__main__':
+    unittest.main()
