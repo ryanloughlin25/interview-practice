f8f7d955a6eec8838ee7b4a4854812de5224138f
Sean Grady <vedicmonk@gmail.com>
2016-12-17 12:28:12 -0800

    more tests, bad attempt at satisying all base cases

--- leetcode_problems/median_of_two_sorted_arrays/sean/median_of_arrays.py
+++ leetcode_problems/median_of_two_sorted_arrays/sean/median_of_arrays.py
@@ -1,29 +1,52 @@
-def find_median(array1, array2):
-    if not array1 and not array2:
-        return None
-    if len(array1) == 1 and len(array2) == 1:
-        return (array1[0] + array2[0]) / 2
-    if not array1:
-        return find_median_single_array(array2)
-    if not array2:
-        return find_median_single_array(array1)
-    array1_len = len(array1)
-    array2_len = len(array2)
-    array1_middle = array1[array1_len // 2]
-    array2_middle = array2[array2_len // 2]
-    if array1_middle > array2_middle:
-        new_array1 = array1[:array1_len // 2]
-        new_array2 = array2[array2_len // 2:]
-        return find_median(new_array1, new_array2)
-    else:
-        new_array1 = array1[array1_len // 2:]
-        new_array2 = array2[:array2_len // 2]
-        return find_median(new_array1, new_array2)
+class Solution:
+    def findMedianSortedArrays(self, array1, array2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: float
+        """
+        if not array1 and not array2:
+            return None
+        if len(array1) == 1 and len(array2) == 1:
+            return (array1[0] + array2[0]) / 2.0
+        if len(array1) + len(array2) < 5:
+            new_array = self.merge_sorted_arrays(array1, array2)
+            return find_median_single_array(new_array)
+        if not array1:
+            return self.find_median_single_array(array2)
+        if not array2:
+            return self.find_median_single_array(array1)
+        array1_len = len(array1)
+        array2_len = len(array2)
+        array1_median = self.find_median_single_array(array1)
+        array2_median = self.find_median_single_array(array2)
+        if array1_median > array2_median:
+            new_array1 = array1[:(array1_len // 2)]
+            new_array2 = array2[array2_len // 2:]
+            return self.findMedianSortedArrays(new_array1, new_array2)
+        elif array2_median > array1_median:
+            new_array1 = array1[array1_len // 2:]
+            new_array2 = array2[:(array2_len // 2)]
+            return self.findMedianSortedArrays(new_array1, new_array2)
+        elif array2_median == array1_median:
+            return array1_median
 
-def find_median_single_array(array):
-    if len(array) % 2 == 0:
-        middle = len(array) // 2
-        return ((array[middle - 1] + array[middle]) / 2)
-    else:
-        middle = len(array) // 2
-        return array[middle]
+    def find_median_single_array(self, array):
+        if len(array) % 2 == 0:
+            middle = len(array) // 2
+            return ((array[middle - 1] + array[middle]) / 2.0)
+        else:
+            middle = len(array) // 2
+            return array[middle]
+
+    def merge_sorted_arrays(self, array1, array2):
+        i = j = 0
+        new_array = []
+        while i < len(array1)-1 or j < len(array2)-1:
+            if array1[i] > array2[j]:
+                new_array.append(array1[i])
+                if i < (len(array1) - 1): i += 1
+            else:
+                new_array.append(array2[j])
+                if j < (len(array2) - 1): j += 1
+        return new_array
--- leetcode_problems/median_of_two_sorted_arrays/sean/tests.py
+++ leetcode_problems/median_of_two_sorted_arrays/sean/tests.py
@@ -1,53 +1,66 @@
 import unittest
-from median_of_arrays import find_median, find_median_single_array
+from median_of_arrays import Solution
 
 
 class MedianOfTwoSortedArraysTestCase(unittest.TestCase):
+    def setUp(self):
+        self.sol = Solution()
+
     def test_empty_arrays(self):
         array1 = []
         array2 = []
-        self.assertEqual(find_median(array1, array2), None)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), None)
 
     def test_array1_one_number(self):
         array1 = [5]
         array2 = []
-        self.assertEqual(find_median(array1, array2), 5)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 5)
 
     def test_array2_one_number(self):
         array1 = []
         array2 = [5]
-        self.assertEqual(find_median(array1, array2), 5)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 5)
 
     def test_two_single_numbers(self):
         array1 = [4]
         array2 = [3]
-        self.assertEqual(find_median(array1, array2), 3.5)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 3.5)
 
     def test_short_asymmetric_arrays(self):
         array1 = [1,3]
         array2 = [2]
-        self.assertEqual(find_median(array1, array2), 2)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 2)
 
     def test_even_array1(self):
         array1 = [1,2,3,4]
         array2 = []
-        self.assertEqual(find_median(array1, array2), 2.5)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 2.5)
 
     def test_even_array2(self):
         array1 = []
         array2 = [1,2,3,4]
-        self.assertEqual(find_median(array1, array2), 2.5)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 2.5)
 
     def test_odd_array1(self):
         array1 = [1,2,3,4,5]
         array2 = []
-        self.assertEqual(find_median(array1, array2), 3)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 3)
 
     def test_odd_array2(self):
         array1 = []
         array2 = [1,2,3,4,5]
-        self.assertEqual(find_median(array1, array2), 3)
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 3)
 
+    def test_two_disjoint_even_arrays(self):
+        array1 = [1,2]
+        array2 = [3,4]
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 2.5)
+
+    def test_overlapping_uneven_arrays(self):
+        array1 = [1,2]
+        array2 = [1,2,3]
+        #import pdb;pdb.set_trace()
+        self.assertEqual(self.sol.findMedianSortedArrays(array1, array2), 2)
 
 if __name__ == '__main__':
     unittest.main()
