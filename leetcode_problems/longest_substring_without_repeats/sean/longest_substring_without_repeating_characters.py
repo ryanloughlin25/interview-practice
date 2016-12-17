4e302263ea47406302230a544e750fa4a391898e
Sean Grady <vedicmonk@gmail.com>
2016-12-16 17:59:48 -0800

    optimized longest_substring, is now only n steps

--- leetcode_problems/longest_substring_without_repeats/sean/longest_substring_without_repeating_characters.py
+++ leetcode_problems/longest_substring_without_repeats/sean/longest_substring_without_repeating_characters.py
@@ -6,12 +6,16 @@ def get_longest_length(string):
     char_dict = {}
     max_len = 0
     j = 0
+    i = 0
     while j < len(string):
         char = string[j]
-        if char in char_dict:
-            char_dict.remove(char)
+        if char in char_dict and char_dict[char] >= i:
+            i = char_dict[char] + 1
+            char_dict[char] = j
+            max_len = max(max_len, j - i + 1)
+            j += 1
         else:
-            char_dict.add(char)
-            max_len = max(max_len, len(char_set))
+            char_dict[char] = j
+            max_len = max(max_len, j - i + 1)
             j += 1
     return max_len
