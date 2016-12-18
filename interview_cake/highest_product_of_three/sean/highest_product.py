91271b60de158ea860593a699a6979f03b8765eb
Sean Grady <vedicmonk@gmail.com>
2016-12-17 21:35:17 -0800

    oh god this is terrible I give up

--- interview_cake/highest_product_of_three/sean/highest_product.py
+++ interview_cake/highest_product_of_three/sean/highest_product.py
@@ -2,8 +2,21 @@ import operator
 import functools
 
 def find_three(integers):
+    if len(integers) <= 3:
+        return functools.reduce(operator.mul, integers, 1)
     highest = [0,0,0]
+    lowest = [0,0,0]
+    num_pos = num_neg = num_zer = 0
     for num in integers:
+        lowest.append(num)
         highest.append(num)
         highest.remove(min(highest))
+        lowest.remove(max(lowest))
+        if num > 0:
+            num_pos += 1
+        elif num < 0:
+            num_neg += 1
+        else:
+            num_zer += 1
+    if num_pos < 3:
     return functools.reduce(operator.mul, highest, 1)
