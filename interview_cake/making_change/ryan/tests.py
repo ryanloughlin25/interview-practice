fa959eb1c013da682c0f899b058b629fbd967cca
rloughlin <rbloughlin@gmail.com>
2016-12-21 10:50:08 -0800

    replace assertEqual with assertCountEqual in making change tests

--- interview_cake/making_change/ryan/tests.py
+++ interview_cake/making_change/ryan/tests.py
@@ -4,25 +4,26 @@ from making_change import making_change
 
 class MakingChangeTestCase(unittest.TestCase):
     def test_zero_ways(self):
-        self.assertEqual(making_change(1, [2]), [])
+        self.assertCountEqual(making_change(1, [2]), [])
 
     def test_one_way(self):
-        self.assertEqual(making_change(3, [3]), [[3]])
+        self.assertCountEqual(making_change(3, [3]), [[3]])
 
     def test_simple(self):
-        self.assertEqual(making_change(2, [1,2]), [[1,1], [2]])
+        #import pdb; pdb.set_trace()
+        self.assertCountEqual(making_change(2, [1,2]), [[1,1], [2]])
 
     def test_blah(self):
-        self.assertEqual(making_change(3, [1,2]), [[1,1,1], [1,2]])
+        self.assertCountEqual(making_change(3, [1,2]), [[1,1,1], [1,2]])
 
     def test_problem_statement_example(self):
-        self.assertEqual(
+        self.assertCountEqual(
             making_change(4, [1,2,3]),
             [[1,1,1,1], [1,1,2], [1,3], [2,2]],
         )
 
     def test_amount_not_divisible_by_any_denomination(self):
-        self.assertEqual(
+        self.assertCountEqual(
             making_change(11, [2,3,5]),
             [[2,2,2,2,3], [2,2,2,5], [2,3,3,3], [3,3,5]],
         )
