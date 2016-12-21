6622ae86a98eb64177941a2b55bf3c4730f1d19f
rloughlin <rbloughlin@gmail.com>
2016-12-21 10:51:31 -0800

    implement non recursive version of making change

--- interview_cake/making_change/ryan/making_change.py
+++ interview_cake/making_change/ryan/making_change.py
@@ -1,4 +1,5 @@
 from itertools import islice
+from collections import defaultdict
 
 """
 bother, I've implemented a function to return the set of
@@ -19,3 +20,28 @@ def making_change(amount, denominations):
             for subproblem in subproblems:
                 result.append([denomination] + subproblem)
     return result
+
+
+def making_change_subproblem(amount, denomination, subresults):
+    result = []
+    for subresult in subresults:
+        result.append(subresult + [denomination])
+    if amount == denomination:
+        result.append([denomination])
+    return result
+
+
+def making_change(amount, denominations):
+    results = defaultdict(list)
+
+    for denomination in denominations:
+        for sub_amount in range(denomination, amount + 1):
+            subresults = making_change_subproblem(
+                sub_amount,
+                denomination,
+                results[sub_amount - denomination],
+            )
+            for subresult in subresults:
+                results[sub_amount].append(subresult)
+
+    return results[amount]
