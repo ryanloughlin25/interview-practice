3d897ff74ca8557e0d20fb88ed6b12e9f23e0420
Jess <jessica.greben1+github2@gmail.com>
2016-12-27 11:19:28 -0800

    add solution to buy stock ic prob with tests

---
+++ interview_cake/buy_stocks/jessicas_solution/making_money.py
@@ -0,0 +1,44 @@
+def max_profit(stock_prices):
+    """
+    what is the max profit possible between two prices
+    of stock assuming you must buy before you can sell
+    in O(n)^2 time
+    """
+    max_profit = 0
+
+    for index, current_price in enumerate(stock_prices):
+        current_index = index
+        max_index = len(stock_prices)
+
+        for i in range(current_index+1, max_index):
+            selling_profit = stock_prices[i] - current_price
+
+            if selling_profit > max_profit:
+                max_profit = selling_profit
+
+    return max_profit
+
+def max_profit_faster(stock_prices):
+    """
+    make money faster in O(n) time
+    buy low then sell high later for max profits
+    """
+    if len(stock_prices) is 0:
+        return 0
+
+    open_price = stock_prices[0]
+    low_price = open_price
+    high_price = open_price
+    max_profit = high_price - low_price
+
+    for price in stock_prices:
+        sell_higher = price - low_price > max_profit
+        buy_lower = price < low_price
+
+        if sell_higher:
+            max_profit = price - low_price
+            high_price = price
+        if buy_lower:
+            low_price = price
+
+    return max_profit
